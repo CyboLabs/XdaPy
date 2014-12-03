8a41c8cb1b82198fc35fa8f5169b19e444e42848
Anthony King <anthonydking@gmail.com>
2014-12-03 15:11:11 +0000

    add in requires_login decorator
    also add a default_login method for the decorator
    to fall back on to

--- XdaPy/user.py
+++ XdaPy/user.py
@@ -16,6 +16,10 @@
 # along with XdaPy.  If not, see <http://www.gnu.org/licenses/>.
 
 from .base import XdaBase
+from .decorators import requires_login
+
+from getpass import getpass
+import sys
 
 
 class User(XdaBase):
@@ -30,9 +34,22 @@ class User(XdaBase):
         cookies = self.xda.requests.get_cookies(r)
         self.xda.session.set_session(username, cookies)
 
+    @requires_login
     def logout(self):
         method = "GET"
         url = "/v1/user/logout"
         r = self.xda.requests.make_request(method, url)
         assert r.status == 200
         self.xda.session.remove_session()
+
+    def default_login(self):
+        """Call for the `requires_login` decorator
+
+        override this to use custom methods for getting user input
+        """
+        sys.stdout.write("What is your username?\n")
+        u = sys.stdin.readline().strip()
+        p = getpass("What is your password: ")
+        print(u)
+        print(p)
+        self.login(u, p)
