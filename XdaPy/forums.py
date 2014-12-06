5cd845c7496bbf2bad413ded9f51274b339475be
Anthony King <anthonydking@gmail.com>
2014-12-06 16:07:19 +0000

    add forum access points

--- XdaPy/forums.py
+++ XdaPy/forums.py
@@ -16,7 +16,66 @@
 # along with XdaPy.  If not, see <http://www.gnu.org/licenses/>.
 
 from .base import XdaBase
+from .decorators import login_required
 
 
 class Forums(XdaBase):
-    pass
+
+    def forums(self):
+        method = "GET"
+        url = "/v1/forums"
+        return self.xda.requests.basic_request(method, url)
+
+    def children(self, forum_id):
+        method = "GET"
+        url = "/v1/forums/children"
+        d = {"forumid": forum_id}
+        return self.xda.requests.basic_request(method, url, body=d)
+
+    def find_by_device(self, query):
+        method = "GET"
+        url = "/v1/forums/findbydevice"
+        d = {"search": query}
+        return self.xda.requests.basic_request(method, url, body=d)
+
+    def general(self):
+        method = "GET"
+        url = "/v1/forums/general"
+        return self.xda.requests.basic_request(method, url)
+
+    def newest(self):
+        method = "GET"
+        url = "/v1/forums/newest"
+        return self.xda.requests.basic_request(method, url)
+
+    @login_required
+    def subscribed(self):
+        method = "GET"
+        url = "/v1/forums/subscribed"
+        return self.xda.requests.basic_request(method, url)
+
+    def top(self):
+        method = "GET"
+        url = "/v1/forums/top"
+        return self.xda.requests.basic_request(method, url)
+
+    @login_required
+    def subscribe(self, forum_id):
+        method = "POST"
+        url = "/v1/forums/subscribe"
+        d = {"forumid": forum_id}
+        return self.xda.requests.basic_request(method, url, body=d)
+
+    @login_required
+    def mark_read(self, forum_id):
+        method = "POST"
+        url = "/v1/forums/markread"
+        d = {"forumid": forum_id}
+        return self.xda.requests.basic_request(method, url, body=d)
+
+    @login_required
+    def unsubscribe(self, forum_id):
+        method = "DELETE"
+        url = "/v1/forums/unsubscribe"
+        d = {"forumid": forum_id}
+        return self.xda.requests.basic_request(method, url, body=d)
