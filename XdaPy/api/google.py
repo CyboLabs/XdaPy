60132e3f1ad63edc20133957ff56926d6cf384f6
Anthony King <anthonydking@gmail.com>
2014-12-10 17:58:43 +0000

    add documentation + instructions for google api

--- XdaPy/api/google.py
+++ XdaPy/api/google.py
@@ -22,6 +22,16 @@ from ..decorators import check_session
 
 
 class Google(XdaBase):
+    """Handles all the requests for getting Google access token
+
+    to use this, you must first create a project at the developer
+    console (https://console.developers.google.com).
+    go to "APIs & auth", and disable all APIs. then go to
+    credentials, click "Create new Client ID", and select
+    "Installed application" -> "Other".
+    your Client ID and Client Secret can then be added to the session
+    by doing `x.session.google_session.set_client("id", "secret")`.
+    """
     def __init__(self, xda):
         super(Google, self).__init__(xda)
         self.host = "accounts.google.com"
@@ -30,6 +40,14 @@ class Google(XdaBase):
 
     @check_session(["client_id"])
     def get_user_code(self):
+        """Get required data for getting a token
+
+        gets a JSON object with device/user codes, the verification
+        url, and the interval for polling times.
+
+        See Also:
+            https://developers.google.com/accounts/docs/OAuth2ForDevices#obtainingacode
+        """
         method = "POST"
         url = "/o/oauth2/device/code"
         body = {"client_id": self.session.client_id,
@@ -39,6 +57,13 @@ class Google(XdaBase):
 
     @check_session(["client_id", "client_secret"])
     def get_tokens(self, device_code):
+        """get access and refresh tokens
+
+        gets a JSON object with the access/refresh tokens.
+
+        See Also:
+            https://developers.google.com/accounts/docs/OAuth2ForDevices#obtainingatoken
+        """
         method = "POST"
         url = "/o/oauth2/token"
         body = {"client_id": self.session.client_id,
@@ -50,6 +75,13 @@ class Google(XdaBase):
 
     @check_session(["client_id", "client_secret", "refresh_token"])
     def refresh_tokens(self):
+        """refresh the access token
+
+        gets a JSON object with the access token in.
+
+        See Also:
+            https://developers.google.com/accounts/docs/OAuth2ForDevices#refreshtoken
+        """
         method = "POST"
         url = "/o/oauth2/token"
         body = {"client_id": self.session.client_id,
