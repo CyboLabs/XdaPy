2f3ba59345b4c47acb253265375a4d082ff6d591
Anthony King <anthonydking@gmail.com>
2014-12-10 17:44:58 +0000

    refactor recaptcha
    use make_request to handle anything that doesn't need to return an url
    
    commonise some parts (get_between, get_data)

--- XdaPy/api/recaptcha.py
+++ XdaPy/api/recaptcha.py
@@ -18,6 +18,14 @@
 from ..base import XdaBase
 
 
+def get_between(body, start, end):
+    if type(body) is bytes:
+        body = body.decode('utf-8')
+    s = body.find(start) + len(start)
+    e = body.find(end, s)
+    return body[s:e]
+
+
 class Recaptcha(XdaBase):
     def __init__(self, xda):
         super(Recaptcha, self).__init__(xda)
@@ -25,30 +33,27 @@ class Recaptcha(XdaBase):
         self.public_key = "6LcVyfcSAAAAAG1QaNlcl6o84brVL9sCVF707V8Q"
 
     def get_challenge(self):
+        method = "GET"
         url = "/recaptcha/api/challenge"
-        params = "k=%s" % self.public_key
-        url = "?".join((url, params))
-        d = self.xda.requests.get(self.host, url).read()
-        d = d.decode('utf-8') if type(d) == bytes else d
-        start = d.find("challenge : ") + 13
-        end = d.find("',\n", start)
-        return d[start:end]
+        body = {"k": self.public_key}
+        d = self.xda.requests.make_request(
+            method, url, body=body, host=self.host).read()
+        return get_between(d, "challenge : '", "',")
 
     def get_token(self, t_type, challenge=None):
         if challenge is None:
             challenge = self.get_challenge()
         t_type = t_type.lower()
         assert t_type in ["image", "audio"]
+
+        method = "GET"
         url = "/recaptcha/api/reload"
-        params = "c=%s&k=%s&type=%s" % (
-            challenge, self.public_key, t_type
-        )
-        url = "?".join((url, params))
-        d = self.xda.requests.get(self.host, url).read()
-        d = d.decode('utf-8') if type(d) == bytes else d
-        start = d.find("('") + 2
-        end = d.find("', ", start)
-        return d[start:end]
+        body = {"c": challenge,
+                "k": self.public_key,
+                "type": t_type}
+        d = self.xda.requests.make_request(
+            method, url, body=body, host=self.host).read()
+        return get_between(d, "('", "',")
 
     def get_image_token(self, challenge=None):
         return self.get_token("image", challenge=challenge)
@@ -56,26 +61,23 @@ class Recaptcha(XdaBase):
     def get_audio_token(self, challenge=None):
         return self.get_token("audio", challenge=challenge)
 
-    def get_image(self, challenge=None, fetch=False):
+    def get_data(self, t_type, challenge=None, fetch=False):
+        t_type = t_type.lower()
+        assert t_type in ["image", "audio"]
         if challenge is None:
-            challenge = self.get_image_token()
-        url = "/recaptcha/api/image"
+            challenge = self.get_token(t_type)
+
+        url = "/recaptcha/api/%s" % t_type
         params = "c=%s" % challenge
         url = "?".join((url, params))
         if fetch:
-            img = self.xda.requests.get(self.host, url).read()
+            data = self.xda.requests.get(self.host, url).read()
         else:
-            img = None
-        return url, img
+            data = None
+        return self.host + url, data
+
+    def get_image(self, challenge=None, fetch=False):
+        return self.get_data("image", challenge, fetch)
 
     def get_audio(self, challenge=None, fetch=False):
-        if challenge is None:
-            challenge = self.get_audio_token()
-        url = "/recaptcha/api/audio"
-        params = "c=%s" % challenge
-        url = "?".join((url, params))
-        if fetch:
-            audio = self.xda.requests.get(self.host, url).read()
-        else:
-            audio = None
-        return url, audio
+        return self.get_data("audio", challenge, fetch)
